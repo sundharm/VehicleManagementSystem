24b3174d5c3ae38c05848cd8969691583a5d06f0
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-09-11 15:39:34 +1000

    Added models
    Added vehicle and car model classes

---
+++ VehicleManagementSystem/Models/Car.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 
+ * A class for the Car vehicle type, which inherits the
+ * vehicle abstract class, plus has it's own specifications.
+ *
+ */
+
+
+namespace VehicleManagementSystem.Models
+{
+    public class Car : Vehicle
+    {
+        public string Engine { get; set; }
+
+        public int NumOfDoors { get; set; }
+
+        public int NumOfWheels { get; set; }
+
+        public string BodyType { get; set; }
+
+        //assigns the vehicle class with the "type" of vehicle
+        public Car(
+            string Make,
+            string Model,
+            string Engine,
+            int NumOfDoors,
+            int numOfWheels,
+            string bodyType) : base("car", Make, Model)
+        {
+            this.NumOfDoors = NumOfDoors;
+            this.NumOfWheels = numOfWheels;
+            this.BodyType = bodyType;
+            this.Engine = Engine;
+        }
+    }
+}
---
+++ VehicleManagementSystem/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * 
+ * An abstract vehicle class with the features common to 
+ * all vehicles. Any sub category of vehicle (car,bike) can
+ * simply inherit this class along with their category
+ * specific features.
+ * 
+ */
+
+namespace VehicleManagementSystem.Models
+{
+    public abstract class Vehicle
+    {
+        public int VehicleId { get; set; }
+
+        public string VehicleType { get; set; }
+
+        public string VehicleMake { get; set; }
+
+        public string VehicleModel { get; set; }
+
+        protected Vehicle(string VehicleType, string VehicleMake, string VehicleModel)
+        {
+            this.VehicleType = VehicleType;
+            this.VehicleMake = VehicleMake;
+            this.VehicleModel = VehicleModel;
+        }
+    }
+}
