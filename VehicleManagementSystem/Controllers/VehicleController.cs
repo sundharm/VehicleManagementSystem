033f34987d02529b1ff1bacd8c4db497dc173868
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-09-12 08:11:00 +1000

    Added logic for controllers
    Controllers can now respond to post and get requests

--- VehicleManagementSystem/Controllers/VehicleController.cs
+++ VehicleManagementSystem/Controllers/VehicleController.cs
@@ -1,33 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using VehicleManagementSystem.Models;
+using VehicleManagementSystem.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace VehicleManagementSystem.Controllers
 {
-    [Route("api/[controller]")]
     public class VehicleController : Controller
     {
-        // GET: api/values
+
         [HttpGet]
         [Route("api/vehicle/get-all-vehicles")]
         public IEnumerable<Vehicle> Get()
         {
-            List<Vehicle> list = new List<Vehicle>() ;
-            return list;
+            return VehicleService.GetAllVehicles();
         }
 
-        
-        // POST api/values
         [HttpPost]
         [Route("api/vehicle/add-vehicle")]
-        public void Post([FromBody] Vehicle vehicle)
+        public IActionResult Post([FromBody]JsonElement vehicle)
         {
-            
+            try
+            {
+                VehicleService.ExtractVehicleDetails(vehicle);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return Ok();
         }
 
 
---
+++ VehicleManagementSystem/Services/VehicleService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using VehicleManagementSystem.Models;
+
+/*
+ *
+ * Service class to separate the insertion/retrieval logic
+ * of data (in memory now). This can later be modified to
+ * work with a database.
+ *
+ */
+namespace VehicleManagementSystem.Services
+{
+    public class VehicleService
+    {
+        private static List<Vehicle> vehicles = new List<Vehicle>();
+
+        //Adds a new vehicle and stores in current instance's memory
+        private static Boolean AddNewVehicle(Vehicle vehicle)
+        {
+            try
+            {
+                //vehicle id is nothing but the current size of list plus 1
+                vehicle.VehicleId = vehicles.Count + 1;
+                vehicles.Add(vehicle);
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //returns the current instance's list of vehicles
+        public static List<Vehicle> GetAllVehicles()
+        {
+            return vehicles;
+        }
+
+
+        //Function to deserialize JSON object and extract vehicle details
+        public static void ExtractVehicleDetails(JsonElement VehicleJson)
+        {
+            string vehicleType = VehicleJson.GetProperty("Type").ToString();
+            string vehicleMake = VehicleJson.GetProperty("Make").ToString();
+            string vehicleModel = VehicleJson.GetProperty("Model").ToString();
+            string engine = VehicleJson.GetProperty("Engine").ToString();
+            int numOfDoors = int.Parse(VehicleJson.GetProperty("NumOfDoors").ToString());
+            int numOfWheels = int.Parse(VehicleJson.GetProperty("NumOfWheels").ToString());
+            string bodyType = VehicleJson.GetProperty("BodyType").ToString();
+
+            Vehicle vehicle;
+
+            //Check for the type of vehicle here and create corresponding object
+            //Additional functionality for different types can be added
+            if (vehicleType.Equals("car"))
+            {
+                vehicle = new Car(vehicleMake,
+                                  vehicleModel,
+                                  engine,
+                                  numOfDoors,
+                                  numOfWheels,
+                                  bodyType);
+            }
+            else
+            {
+                throw new Exception("Invalid type of vehicles");
+            }
+
+            try
+            {
+                AddNewVehicle(vehicle);
+            }
+            catch
+            {
+                throw;
+            }
+
+        }   
+    }
+}
