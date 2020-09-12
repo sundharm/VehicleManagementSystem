52c62314f475ef5267c82d3208d90b462884bba4
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-09-12 16:42:51 +1000

    Handled form changes
    Added functions to handle the changes in form fields

--- VehicleManagementSystem/ClientApp/src/components/AddNewCar.js
+++ VehicleManagementSystem/ClientApp/src/components/AddNewCar.js
@@ -6,7 +6,49 @@ import './styles/AddNewCar.css'
 
 export class AddNewCar extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            make: '',
+            model: '',
+            bodyType: '',
+            engine: '',
+            numOfDoors: '',
+            numOfWheels: '',
+        };
+
+    }
+
+    //handling functions for each input
+    handleMake(event) {
+        this.setState({ make: event.target.value })
+    }
+
+    handleModel(event) {
+        this.setState({ model: event.target.value })
+    }
+
+    handleBodyType(event) {
+        this.setState({ bodyType: event.target.value })
+    }
+
+    handleEngine(event) {
+        this.setState({ engine: event.target.value })
+    }
+
+    handleNumberOfDoors(event) {
+        this.setState({ numOfDoors: event.target.value })
+    }
+
+    handleNumberOfWheels(event) {
+        this.setState({ numOfWheels: event.target.value })
+    }
+
     render() {
+        const { make, model, engine,bodyType,numOfDoors, 
+            numOfWheels, } = this.state;
+
         return (
             <div>
                 <h3 className='label'>Add a Car</h3>
@@ -16,16 +58,22 @@ export class AddNewCar extends Component {
                         <Form.Group as={Col} controlId='formMake'>
                             <Form.Control
                                 placeholder='Make'
+                                value = {make}
+                                onChange={this.handleMake.bind(this)}
                             />
                         </Form.Group>
                         <Form.Group as={Col} controlId='formModel'>
                             <Form.Control
                                 placeholder='Model'
+                                value = {model}
+                                onChange={this.handleModel.bind(this)}
                             />
                         </Form.Group>
                         <Form.Group as={Col} controlId='formBodyType'>
                             <Form.Control
                                 placeholder='Body type'
+                                value={bodyType}
+                                onChange={this.handleBodyType.bind(this)}
                             />
                         </Form.Group>
                         </Form.Row>
@@ -33,6 +81,8 @@ export class AddNewCar extends Component {
                         <Form.Group as={Col} controlId='formEngine'>
                             <Form.Control
                                 placeholder='Engine'
+                                value={engine}
+                                onChange={this.handleEngine.bind(this)}
                             />
                         </Form.Group>
                         <Form.Group as={Col} controlId='formNumOfDoors'>
@@ -41,6 +91,8 @@ export class AddNewCar extends Component {
                                 min='0'
                                 max='10'
                                 placeholder='Number of doors'
+                                value={numOfDoors}
+                                onChange={this.handleNumberOfDoors.bind(this)}
                             />
                         </Form.Group>
                         <Form.Group as={Col} controlId='formNumOfWheels'>
@@ -49,6 +101,8 @@ export class AddNewCar extends Component {
                                 min='0'
                                 max='10'
                                 placeholder='Number of wheels'
+                                value={numOfWheels}
+                                onChange={this.handleNumberOfWheels.bind(this)}
                             />
                         </Form.Group>
                     </Form.Row>
@@ -65,6 +119,4 @@ export class AddNewCar extends Component {
             </div>
         );
     }
-
-
 }
