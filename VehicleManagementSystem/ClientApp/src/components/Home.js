77b69f55ba2d0085c0793b1c158d12faa93aa156
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-09-12 10:06:11 +1000

    Added dropdown to homepage
    added a bootstrap dropdown element

--- VehicleManagementSystem/ClientApp/src/components/Home.js
+++ VehicleManagementSystem/ClientApp/src/components/Home.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    );
+        <React.Fragment>
+            <DropdownButton className="drop-down-button" id="dropdown-basic-button" title="Select a vehicle to add">
+                <Dropdown.Item className="car">Car</Dropdown.Item>
+                <Dropdown.Item className="disabled">Bike</Dropdown.Item>
+                <Dropdown.Item className="disabled">Boat</Dropdown.Item>
+            </DropdownButton>
+        </React.Fragment>
+    )
   }
 }
